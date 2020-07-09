3211a8879adc33bf6086fbb86c9001366bbe74ed
Savni Nagarkar <johndoe@example.com>
2020-07-09 14:37:33 -0500

    button sends amount value

--- src/components/AskLadder.js
+++ src/components/AskLadder.js
@@ -5,22 +5,22 @@ function AskLadder (props) {
     //console.log("inside ask ladder function", props);
     if(props.mode == 'on'){
         return <div>
-            <Button amount = '$10' user = {props.userKey} />
-            <Button amount = '$15' user = {props.userKey} />
-            <Button amount = '$20' user = {props.userKey} />
-            <Button amount = '$40' user = {props.userKey} />
-            <Button amount = '$80' user = {props.userKey} />
-            <Button amount = '$150' user = {props.userKey} />
+            <Button amount = {10} user = {props.userKey} mode = 'on'/>
+            <Button amount = {15} user = {props.userKey} mode = 'on'/>
+            <Button amount = {20} user = {props.userKey} mode = 'on'/>
+            <Button amount = {40} user = {props.userKey} mode = 'on'/>
+            <Button amount = {80} user = {props.userKey} mode = 'on'/>
+            <Button amount = {150} user = {props.userKey} mode = 'on'/>
         </div>
     }
     else if(props.mode == 'off'){
         return <div>
-            <Button amount = '$50' user = {props.userKey} />
-            <Button amount = '$80' user = {props.userKey} />
-            <Button amount = '$100' user = {props.userKey} />
-            <Button amount = '$200' user = {props.userKey} />
-            <Button amount = '$400' user = {props.userKey} />
-            <Button amount = '$750' user = {props.userKey} />
+            <Button amount = {50} user = {props.userKey} mode = 'off'/>
+            <Button amount = {80} user = {props.userKey} mode = 'off'/>
+            <Button amount = {100} user = {props.userKey} mode = 'off'/>
+            <Button amount = {200} user = {props.userKey} mode = 'off'/>
+            <Button amount = {400} user = {props.userKey} mode = 'off'/>
+            <Button amount = {750} user = {props.userKey} mode = 'off'/>
         </div>
     }
     else
--- src/components/Button.js
+++ src/components/Button.js
@@ -12,13 +12,21 @@ class Button extends Component {
        // this.setState({startingValue: 50})
         //alert('button clicked');
        // console.log('button clicked');
-        split?.client().track('button');
+         let value = this.props.amount;
+         let userNum = this.props.user;
+         let treatment = this.props.mode;
+         let properties = { user : userNum , amount : value, treatment: treatment};
+         console.log(properties);
+         split?.client().track('button', value, properties);
+        // console.log(this.props.user);
+       // split?.client().track(userNum, 'user', 'donation_button', value, null );
+       // split?.client().track(userNum, 'donation_button', 'donate_button', value, null);
          //split?.client().track({this.props.user}, 'user', 'donate button', {this.props.amount});
     }
 
     render() {
         return(
-            <button onClick = {() => this.clickHandler()}> {this.props.amount}</button>
+            <button onClick = {() => this.clickHandler()}> ${this.props.amount}</button>
         )
 
     }
